a887b5e09ef8d95d9b1751cbc8cdc62b85e79bb6
Ernesto <ernesto.boza.delacruz@gmail.com>
2024-12-09 12:53:00 +0100

    Innitial Commit1

---
+++ force-app/main/default/lwc/speciesTile/speciesTile.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+export default class SpeciesTile extends LightningElement {
+    specie = {
+        Name: "Jazmin",
+        Description: "Olorosa y bonita planta trepadora",
+        Image_URL_c:
+        "https://content.elmueble.com/medio/2023/07/03/00348199-jazmin_00000000_c0f951da_230703160437_1000x1333.jpg",
+        Location__c:"Indoors,Outdoors"
+    };
+}
